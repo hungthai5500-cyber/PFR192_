bfa10bbfead472c12df58a248c009fc5282f0d93
hungthai5500-cyber <hungthai5500@gmail.com>
2026-01-12 16:29:53 +0700

    Add Ex4.c in slot2

---
+++ slot2/Ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    float a, b;
+
+    printf("Nhap a: ");
+    scanf("%f", &a);
+
+    printf("Nhap b: ");
+    scanf("%f", &b);
+
+    printf("Ket qua a + b = %.2f", a + b);
+
+    return 0;
+}
